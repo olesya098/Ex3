604c722b859ebddd3d7a7ff81e9b59ecce850384
Rig3 <olesya092006@yandex.ru>
2025-04-05 21:53:24 +0300

    Создание Камеры

---
+++ app/src/main/java/com/hfad/myapplication/Photo.kt
@@ -0,0 +1,9 @@
+package com.hfad.myapplication
+
+import android.net.Uri
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun Photo(navController: NavController, imageUri: String) {
+}
