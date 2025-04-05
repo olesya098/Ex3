75589f6a2c36841f910797b989b3a60b7d2c4888
Rig3 <olesya092006@yandex.ru>
2025-04-05 20:14:29 +0300

    Create Secreen1

--- app/src/main/java/com/hfad/myapplication/MainActivity.kt
+++ app/src/main/java/com/hfad/myapplication/MainActivity.kt
@@ -33,15 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Olesya!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
 }
